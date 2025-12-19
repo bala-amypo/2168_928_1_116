07a0113e769b50f6e1aa0b33e02ca4318a2ad0ba
bala-amypo <bala@amypo.in>
2025-12-19 05:23:41 +0000

    User commit

---
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Drug Interaction Checker API")
+                        .description("Swagger CRUD APIs")
+                        .version("1.0"))
+                
+                .servers(List.of(
+                        new Server().url("/")
+                ));
+    }
+}
