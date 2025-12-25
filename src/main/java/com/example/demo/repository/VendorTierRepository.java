7450e0ec44bc98e7ccca4bea2bc95c0b9d583cd6
bala-amypo <bala@amypo.in>
2025-12-25 16:18:03 +0000

    User commit

--- src/main/java/com/example/demo/repository/VendorTierRepository.java
+++ src/main/java/com/example/demo/repository/VendorTierRepository.java
@@ -1,7 +1,15 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.VendorTier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface VendorTierRepository extends JpaRepository<Object, Long> {
-    boolean existsByTierName(String name);
+import java.util.Optional;
+
+@Repository
+public interface VendorTierRepository extends JpaRepository<VendorTier, Long> {
+
+    Optional<VendorTier> findByTierName(String tierName);
+
+    Optional<VendorTier> findFirstByActiveTrueOrderByDiscountPercentageDesc();
 }
