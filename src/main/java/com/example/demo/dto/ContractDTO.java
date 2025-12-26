94f8e0b3fe97785859836c69340d4cc2a3b6737c
bala-amypo <bala@amypo.in>
2025-12-26 03:32:06 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/ContractDTO.java
@@ -0,0 +1,53 @@
+package com.example.demo.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class ContractDTO {
+
+    private Long id;
+    private String contractName;
+    private BigDecimal baseContractValue;
+    private LocalDate agreedDeliveryDate;
+    private Long vendorId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContractName() {
+        return contractName;
+    }
+
+    public void setContractName(String contractName) {
+        this.contractName = contractName;
+    }
+
+    public BigDecimal getBaseContractValue() {
+        return baseContractValue;
+    }
+
+    public void setBaseContractValue(BigDecimal baseContractValue) {
+        this.baseContractValue = baseContractValue;
+    }
+
+    public LocalDate getAgreedDeliveryDate() {
+        return agreedDeliveryDate;
+    }
+
+    public void setAgreedDeliveryDate(LocalDate agreedDeliveryDate) {
+        this.agreedDeliveryDate = agreedDeliveryDate;
+    }
+
+    public Long getVendorId() {
+        return vendorId;
+    }
+
+    public void setVendorId(Long vendorId) {
+        this.vendorId = vendorId;
+    }
+}
