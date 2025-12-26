f44bef359f7a6530617ebb66821f379355b97d57
bala-amypo <bala@amypo.in>
2025-12-26 04:05:21 +0000

    User commit

--- src/main/java/security/SecurityConfig.java
+++ src/main/java/security/SecurityConfig.java
@@ -1,11 +1,12 @@
-package security;
+package com.example.demo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -30,11 +31,11 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
                         "/swagger-ui/**",
-                        "/v3/api-docs/**"
+                        "/v3/api-docs/**",
+                        "/auth/**"
                 ).permitAll()
                 .anyRequest().authenticated()
-            )
-            .httpBasic();
+            );
 
         return http.build();
     }
