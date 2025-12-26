a85ed1afae0945340f403d5b0478b7268761397a
bala-amypo <bala@amypo.in>
2025-12-26 09:47:24 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/PenaltyCalculation.java
@@ -0,0 +1,24 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @Builder
+@NoArgsConstructor @AllArgsConstructor
+public class PenaltyCalculation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Contract contract;
+
+    private Integer daysDelayed;
+    private BigDecimal calculatedPenalty;
+    private LocalDateTime calculatedAt = LocalDateTime.now();
+}
