9f7ef91f5b8ccb00754981026d9f4fcd622abf0e
bala-amypo <bala@amypo.in>
2025-12-26 09:41:24 +0000

    User commit

--- src/main/java/com/example/demo/exception/OperationNotAllowedException.java
+++
@@ -1,8 +0,0 @@
-// package com.example.demo.exception;
-
-// public class OperationNotAllowedException extends RuntimeException {
-
-//     public OperationNotAllowedException(String message) {
-//         super(message);
-//     }
-// }
--- src/main/java/com/example/demo/repository/VendorTierRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VendorTierRepository extends JpaRepository<Object, Long> {
-
-    boolean existsByTierName(String name);
-}
