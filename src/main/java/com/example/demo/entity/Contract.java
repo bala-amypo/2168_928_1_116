e62f4de8692b603a3c5c458a951fa15225207b96
bala-amypo <bala@amypo.in>
2025-12-27 16:45:09 +0000

    User commit

--- src/main/java/com/example/demo/entity/Contract.java
+++ src/main/java/com/example/demo/entity/Contract.java
@@ -13,30 +13,31 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Contract {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     @Column(unique = true, nullable = false)
     private String contractNumber;
-    
+
     private String title;
     private String counterpartyName;
     private LocalDate agreedDeliveryDate;
     private BigDecimal baseContractValue;
     private String status;
-    
+
     @Column(updatable = false)
     private LocalDateTime createdAt;
-    
+
     private LocalDateTime updatedAt;
-    
+
     @PrePersist
     protected void onCreate() {
         createdAt = LocalDateTime.now();
         updatedAt = LocalDateTime.now();
     }
-    
+
     @PreUpdate
     protected void onUpdate() {
         updatedAt = LocalDateTime.now();
