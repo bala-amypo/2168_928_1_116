115db4e326c423ecc5cdb1abbbdaaf6e12c2bc73
bala-amypo <bala@amypo.in>
2025-12-28 11:08:35 +0000

    User commit

--- src/main/java/com/example/demo/entity/BreachRule.java
+++ src/main/java/com/example/demo/entity/BreachRule.java
@@ -11,15 +11,20 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BreachRule {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
-    @Column(unique = true)
+
+    @Column(unique = true, nullable = false)
     private String ruleName;
-    
+
     private BigDecimal penaltyPerDay;
+
     private Double maxPenaltyPercentage;
+
     private Boolean active;
+
+    // ⚠️ DO NOT CHANGE THIS NAME — TESTS EXPECT IT
     private Boolean isDefaultRule;
 }
