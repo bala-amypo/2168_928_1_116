f616e3cce3104079d676da7f79b5c5ffd824df61
bala-amypo <bala@amypo.in>
2026-01-02 03:50:27 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -3,7 +3,6 @@ package com.example.demo.entity;
 import lombok.*;
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -13,35 +12,25 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    
     @Column(unique = true, nullable = false)
     private String email;
-
-    @Column(nullable = false)
+    
     private String password;
-
+    
     @ElementCollection(fetch = FetchType.EAGER)
-    @CollectionTable(
-        name = "user_roles",
-        joinColumns = @JoinColumn(name = "user_id")
-    )
-    @Column(name = "role", nullable = false)
-    @Builder.Default
-    private Set<String> roles = new HashSet<>();
-
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private Set<String> roles;
+    
     @Column(updatable = false)
     private LocalDateTime createdAt;
-
+    
     @PrePersist
     protected void onCreate() {
         createdAt = LocalDateTime.now();
-
-        if (roles == null) {
-            roles = new HashSet<>();
-        }
     }
 }
