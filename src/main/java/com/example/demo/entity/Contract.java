9b6cf7087c0b4b54b1f9f14c857cce34c21a2af0
bala-amypo <bala@amypo.in>
2026-01-02 03:26:54 +0000

    User commit

--- src/main/java/com/example/demo/entity/Contract.java
+++ src/main/java/com/example/demo/entity/Contract.java
@@ -1,8 +1,6 @@
 package com.example.demo.entity;
 
-import com.example.demo.enums.ContractStatus;
 import lombok.*;
-
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -11,44 +9,36 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "contracts")
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class Contract {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(nullable = false, unique = true)
+    
+    @Column(unique = true, nullable = false)
     private String contractNumber;
-
+    
     private String title;
     private String counterpartyName;
-
-    @Column(nullable = false)
     private LocalDate agreedDeliveryDate;
-
-    @Column(nullable = false)
     private BigDecimal baseContractValue;
-
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private ContractStatus status;
-
+    private String status;
+    
     @Column(updatable = false)
     private LocalDateTime createdAt;
-
+    
     private LocalDateTime updatedAt;
-
+    
     @PrePersist
-    void onCreate() {
+    protected void onCreate() {
         createdAt = LocalDateTime.now();
         updatedAt = LocalDateTime.now();
     }
-
+    
     @PreUpdate
-    void onUpdate() {
+    protected void onUpdate() {
         updatedAt = LocalDateTime.now();
     }
 }
